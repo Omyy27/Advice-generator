2794c03325e43ea6b1610ae69ef9dd5ab139b09b
Omyy27 <omovisolutions@gmail.com>
2022-04-12 22:49:03 -0500

    First

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function showQuote() {
+    var idAdvice = document.querySelector('.number-quote');
+    var textAdvice = document.querySelector('.quote');
+    let arrTo = [];
+    fetch('https://api.adviceslip.com/advice').then(response => (
+        response.json()
+      ))
+      .then(json => { 
+        var keys = Object.keys(json);
+        keys.forEach(function(key){
+          arrTo.push(json[key]);
+        });
+        arrTo.forEach(element => {
+          console.log(element)
+          idAdvice.innerHTML = element.id 
+          textAdvice.innerHTML = element.advice 
+        });
+      });
+  }
+  showQuote();
+  var btnGenerate = document.querySelector('.btngen');
+  btnGenerate.addEventListener('click', showQuote);
+});
+ 
+
+
